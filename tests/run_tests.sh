04bdbfa2fad70f9649b6ed758e02d02e5a222d91
philheller <philip.heller@student.kit.edu>
2024-09-18 10:20:11 +0200

    Restructure to only keep relevant files in this repo
    - explorative moved to different repo

--- tests/run_tests.sh
+++
@@ -1,9 +0,0 @@
-# todo adapt to new testing structure
-echo "Running tests for Decoding methods."
-
-python greedy_search_test.py
-python beam_search_test.py
-python group_beam_search_test.py
-python contrastive_search_test.py
-
-echo "All tests performed."
