8a6ab94bdc8f37bc3789d1b3896223fc8a757134
philheller <philip.heller@student.kit.edu>
2024-09-19 12:56:57 +0200

    Add first experiment (not implemented yet)

---
+++ experiments/factuality_prompt.py
@@ -0,0 +1,13 @@
+
+
+# setup models
+
+# generate 500 beams for regular beam search
+
+
+# generate 500 beams for semantic beam search
+
+
+# extract the first entities for both
+# -> aggregate most common entity for both
+# visualize most common entity for both
