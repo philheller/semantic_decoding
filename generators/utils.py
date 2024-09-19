d7e12038fc561264ca8a86aecd3b3c551d0d2853
philheller <philip.heller@student.kit.edu>
2024-09-19 12:39:43 +0200

    Add utils for hw stats of nvidia devices

--- generators/utils.py
+++ generators/utils.py
@@ -1,5 +1,6 @@
 import pickle
 import torch
+from pynvml import *
 
 def deep_compare(obj1, obj2, rtol: float = 1e-5):
     if type(obj1) != type(obj2):
@@ -34,3 +35,15 @@ def load_from_pickle_file(path):
                 except EOFError:
                     break
     return objects
+
+def print_gpu_utilization():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    print(f"GPU memory occupied: {info.used//1024**2} MB.")
+
+
+def print_summary(result):
+    print(f"Time: {result.metrics['train_runtime']:.2f}")
+    print(f"Samples/second: {result.metrics['train_samples_per_second']:.2f}")
+    print_gpu_utilization()
